ba28d8bb863e5af628f101520be885830827fa4b
Le Nhat Tung <49211930+lenhattung@users.noreply.github.com>
2024-09-11 10:12:39 +0700

    Update E08_Loop.java

--- E08_Loop.java
+++ E08_Loop.java
@@ -3,39 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication2b;
+package javaapplication2a;
 
 /**
  *
  * @author tungi
  */
 public class E08_Loop {
+
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,5};
-        for(int i=0; i<a.length; i++)
-            System.out.print(a[i]+",");
-        
-        System.out.println();
-        for(int i=0; i<a.length; i++){
-            System.out.print(a[i]+",");
+        int a[] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println("");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println("");
+        for (int x : a);
+        { // thừa ; sau for
+            System.out.print("x"); // Nhầm biến x thành chuỗi x
+        }
+        System.out.println("");
+        for (int x : a) {
+            System.out.print(x + ",");
         }
         
-        // Lưu ý lỗi sai
-        System.out.println();
-        for (int x: a);{ // Thừa ; sau for
-            System.out.print("x"+","); // In ra ký tự thay vì giá trị của biến
+        System.out.println("");
+        for (int x : a) {
+            System.out.print(x+",");
+            x+=100; // x = x + 100;
         }
         
-        System.out.println();
-        for (int i : a) {
-            System.out.print(i+",");
-            i+=100;
-        }
-        
-        System.out.println();
-        for (int i : a) {
-            i+=100;
-            System.out.print(i+",");
+        System.out.println("");
+        for (int x : a) {
+            x+=100; // x = x + 100;
+            System.out.print(x+",");
         }
     }
 }
